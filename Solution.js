37549ef298245131087640ceb5877cd7a3a2f84c
Mohammad Ali Apu <mohammadapu1@gmail.com>
2020-08-23 04:04:26 +0600

    Added right solution file

--- Solution.js
+++ Solution.js
@@ -1,17 +1,5 @@
-let allRead = true;
+let arr = [1, 2, 2, 3, 4, 4, 5];
 
-let notifications = [
-	{message: 'Lorem', read: true},
-	{message: 'Ipsum', read: true},
-	{message: 'Dolor', read: true},
-	{message: 'Sit', read: false},
-	{message: 'Amet', read: true}
-];
+arr = [... new Set(arr)];
 
-// finding by read status
-let n = notifications.find(item => item.read === false);
-
-// if we found any
-if (n) allRead = false;
-
-console.log(allRead);
+console.log(arr);
